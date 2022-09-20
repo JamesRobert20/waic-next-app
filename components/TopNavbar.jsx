d990645f813263a290fa23d090553fd85eebbc83
JamesRobert20 <jmtendamema@gmail.com>
2022-09-19 21:41:26 -0600

    fix

--- components/TopNavbar.jsx
+++ components/TopNavbar.jsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Link from "next/link"
 import { useRouter } from 'next/router'
-import navstyles from '../styles/TopNavbar.module.css'
+import navstyles from '../styles/Navbar.module.css'
 import { useContext } from 'react'
 import UserAuthenticationContext from '../User/userAuthenticator'
 
