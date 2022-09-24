e7971df81ddd79cef8fb590cf06fbf5cf5b804ec
James Mtendamema <jmtendamema@geologicai.com>
2022-09-24 11:44:01 -0600

    commented out component

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,25 @@ import { useContext } from 'react'
 import UserAuthenticationContext from '../User/userAuthenticator'
 
 export default function Home() {
+    /* import FlexibleDocViewer from "../components/FlexibleDocViewer"
+
+    function docViewTest() {
+        const docs = [
+            { uri: "http://localhost:3000/files/percyjack.pdf" },
+            { uri: "http://localhost:3000/files/exception.PNG" },
+            { uri: "http://localhost:3000/files/WAIC.pdf" }, // Local File
+        ];
+
+        console.log("How many times do you render");
+
+        return (
+            <center>
+                <FlexibleDocViewer docs={docs} />
+            </center>
+        )
+    }
+
+    export default docViewTest */
     const { user } = useContext(UserAuthenticationContext);
 
     console.log("preventing prop drilling got: ", user);
